0ef73e219966513dad7449e9b51011844f330158
BakirK <bakir.karovic@gmail.com>
2019-05-22 09:21:49 +0200

    Dodane klase za graficki interface

--- src/ba/unsa/etf/rs/tutorijal8/Main.java
+++ src/ba/unsa/etf/rs/tutorijal8/Main.java
@@ -1,5 +1,45 @@
 package ba.unsa.etf.rs.tutorijal8;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MVCFXML.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -107,3 +147,4 @@ public class Main {
         dao.addDriver(new Driver(name, surname, umcn, birthday, hireDate));
     }
 }
+*/
